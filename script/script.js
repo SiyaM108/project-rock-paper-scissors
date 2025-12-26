889ec1def97aa1d08e5c7ed63b0ae7d581b878e8
SiyaM108 <deprominent@gmail.com>
2025-12-26 19:48:32 +0000

    complete getComputerChoice()

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
 console.log("Hello, World!");
+
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 3) + 1);  // random number from 1 to 3
+    if(choice === 1) {
+        return "rock";
+    }
+    else if(choice === 2) {
+        return "paper";
+    }
+    else if(choice === 3) {
+        return "scissors";
+    }
+}
