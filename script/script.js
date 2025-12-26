15f4b75d87edd180fe4f8bb468038cf69db462ea
SiyaM108 <deprominent@gmail.com>
2025-12-26 22:59:39 +0000

    Write the logic to play a single round

--- script/script.js
+++ script/script.js
@@ -18,7 +18,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = parseInt(prompt("Please enter your choice:\n1: rock\n2: paper\n3:scissors\n"));
+    let humanChoice = parseInt(prompt("Please enter your choice:\n1: rock\n2: paper\n3: scissors\n"));
         if(humanChoice === 1) {
         return "rock";
     }
@@ -29,3 +29,27 @@ function getHumanChoice() {
         return "scissors";
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if(humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beat Paper");
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
